17ca35833dc0f0f49a292a624c003cc427b15306
Richard Byrne <richardbyrne1995@gmail.com>
2020-11-25 16:02:47 +0000

    Animate A and B buttons when pressing 'a' and 'b'

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -1,10 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    //Checks which keys are being pressed
+    function checkDownKey(e) {
+        if (e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+            animateDPad(e.key);
+        } else if (e.key === 'a' || e.key === 'b') {
+            animateABButton(e.key);
+        }
+    }
+
+    function checkUpKey(e) {
+        if (e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+            resetDPad(e.key);
+        } else if (e.key === 'a' || e.key === 'b') {
+            resetABButton(e.key);
+        }
+    }
+
     //D-Pad Animation
     const innerDPad = $("#inner-d-pad")
 
     function animateDPad(key) {
         if (key === 'ArrowLeft') {
-            $(innerDPad).removeClass().addClass('d-pad-left-animation'); 
+            $(innerDPad).removeClass().addClass('d-pad-left-animation');
         } else if (key === 'ArrowRight') {
             $(innerDPad).removeClass().addClass('d-pad-right-animation');
         } else if (key === 'ArrowUp') {
@@ -13,25 +30,43 @@ $(document).ready(function() {
             $(innerDPad).removeClass().addClass('d-pad-down-animation');
         }
     }
-    
-    function removeAnimation(key) {
-        $(innerDPad).removeClass('d-pad-left-animation d-pad-right-animation');
-    }
-    
-    function callAnimateOnKeyPress(e) {
-        if (e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
-             animateDPad(e.key);
-        } else {
-            removeAnimation();
-        }
-    }
-    
-    function resetDPadOnKeyUp(e) {
-        if (e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+
+    function resetDPad(key) {
+        if (key === 'ArrowDown' || key === 'ArrowUp' || key === 'ArrowLeft' || key === 'ArrowRight') {
             $(innerDPad).removeClass();
         }
     }
+
+
+    // A & B Button Animation
+    const aButtonTop = $("#A-button-top");
+    const aButtonSide = $("#A-button-side");
+    const bButtonTop = $("#B-button-top");
+    const bButtonSide = $("#B-button-top");
+
+    function animateABButton(key) {
+        if (key === 'a') {
+            $(aButtonTop).addClass("a-b-button-animation");
+        } else if (key === 'b') {
+            $(bButtonTop).addClass("a-b-button-animation");
+        }
+    }
+
+    function resetABButton(key) {
+        if (key === 'a') {
+            $(aButtonTop).removeClass();
+            $(aButtonSide).removeClass();
+        } else if (key === 'b') {
+            $(bButtonTop).removeClass()
+            $(bButtonSide).removeClass();
+        }
+    }
+
+
+    //Select & Start Button Animation
     
-    $(document).keydown(callAnimateOnKeyPress);
-    $(document).keyup(resetDPadOnKeyUp);
+
+    //Calling Listeners
+    $(document).keydown(checkDownKey);
+    $(document).keyup(checkUpKey);
 })
