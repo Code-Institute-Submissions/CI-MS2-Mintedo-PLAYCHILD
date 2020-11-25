46e426897db6884d81dbfc71c866ca8694d7061b
Richard Byrne <richardbyrne1995@gmail.com>
2020-11-25 14:50:30 +0000

    Load helper.js after document load

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -1,32 +1,34 @@
-function animate(key) {
-    if (key === 'ArrowLeft') {
-        $("#inner-d-pad").removeClass().addClass('d-pad-left-animation'); 
-    } else if (key === 'ArrowRight') {
-        $("#inner-d-pad").removeClass().addClass('d-pad-right-animation');
-    } else if (key === 'ArrowUp') {
-        $("#inner-d-pad").removeClass().addClass('d-pad-up-animation');
-    } else if (key === 'ArrowDown') {
-        $("#inner-d-pad").removeClass().addClass('d-pad-down-animation');
+$(document).ready(function() {
+    function animate(key) {
+        if (key === 'ArrowLeft') {
+            $("#inner-d-pad").removeClass().addClass('d-pad-left-animation'); 
+        } else if (key === 'ArrowRight') {
+            $("#inner-d-pad").removeClass().addClass('d-pad-right-animation');
+        } else if (key === 'ArrowUp') {
+            $("#inner-d-pad").removeClass().addClass('d-pad-up-animation');
+        } else if (key === 'ArrowDown') {
+            $("#inner-d-pad").removeClass().addClass('d-pad-down-animation');
+        }
     }
-}
-
-function removeAnimation(key) {
-    $("#inner-d-pad").removeClass('d-pad-left-animation d-pad-right-animation');
-}
-
-function animateOnKeyPress(e) {
-    if (e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
-         animate(e.key);
-    } else {
-        removeAnimation();
+    
+    function removeAnimation(key) {
+        $("#inner-d-pad").removeClass('d-pad-left-animation d-pad-right-animation');
     }
-}
-
-function resetOnKeyUp() {
-    $("#inner-d-pad").removeClass()
-}
-
-
-
-$(document).keydown(animateOnKeyPress);
-$(document).keyup(resetOnKeyUp);
+    
+    function animateOnKeyPress(e) {
+        if (e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+             animate(e.key);
+        } else {
+            removeAnimation();
+        }
+    }
+    
+    function resetOnKeyUp() {
+        $("#inner-d-pad").removeClass()
+    }
+    
+    
+    
+    $(document).keydown(animateOnKeyPress);
+    $(document).keyup(resetOnKeyUp);
+})
