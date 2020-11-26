91839e8522bc926e00efd4a3cb3766dd39a9af28
Richard Byrne <richardbyrne1995@gmail.com>
2020-11-26 16:16:59 +0000

    Create rough draft start screen

--- assets/js/startScene.js
+++ assets/js/startScene.js
@@ -8,7 +8,11 @@ class StartScene extends Phaser.Scene {
     }
 
     create() {
-        const screen = this.add.image(0,0, 'start').setOrigin(0);
+        // const screen = this.add.image(0,0, 'start').setOrigin(0);
+        const introText1 = this.add.text(24, 50, `Gather the Gems`, { fontSize: '15px', fill: '#fff' });
+        const introText2 = this.add.text(20, 65, `Avoid the Snakes`, { fontSize: '15px', fill: '#fff' });
+        const startPrompt1 = this.add.text(44, 90, `Press Enter`, { fontSize: '15px', fill: '#fff' });
+        const startPrompt2 = this.add.text(60, 105, `To Start`, { fontSize: '15px', fill: '#fff' });
         
         this.input.keyboard.on('keydown', () => {
             this.scene.stop('StartScene');
