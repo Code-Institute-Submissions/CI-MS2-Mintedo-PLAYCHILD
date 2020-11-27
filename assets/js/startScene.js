1ce1808e13db0ebfdbb08f4191653809f07bf159
Richard Byrne <richardbyrne1995@gmail.com>
2020-11-27 16:14:13 +0000

    Add sounds to startup and game start

--- assets/js/startScene.js
+++ assets/js/startScene.js
@@ -5,10 +5,15 @@ class StartScene extends Phaser.Scene {
 
     preload() {
         this.load.image('start', 'assets/images/placeholder.jpg');
+        this.load.audio('ping', ['assets/sound/Gameboy_Startup_Sound.ogg']);
+        this.load.audio('startSound', ['assets/sound/start-game.ogg']);
     }
 
     create() {
-        // const screen = this.add.image(0,0, 'start').setOrigin(0);
+        const startGameSound = this.sound.add('startSound');
+        const startupSound = this.sound.add('ping');
+        startupSound.play();
+
         const introText1 = this.add.text(24, 50, `Gather the Gems`, { fontSize: '15px', fill: '#fff' });
         const introText2 = this.add.text(20, 65, `Avoid the Snakes`, { fontSize: '15px', fill: '#fff' });
         const startPrompt1 = this.add.text(44, 90, `Press Enter`, { fontSize: '15px', fill: '#fff' });
@@ -18,7 +23,8 @@ class StartScene extends Phaser.Scene {
             if (e.keyCode === 13) {
                 this.scene.stop('StartScene');
                 this.scene.start('GameScene');
+                startGameSound.play();
             };
-    });
-}
+        });
+    }
 }
