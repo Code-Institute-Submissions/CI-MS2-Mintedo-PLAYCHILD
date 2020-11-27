d15f38ab24152c2849ffa11f375fb71747356e88
Richard Byrne <richardbyrne1995@gmail.com>
2020-11-27 21:13:45 +0000

    Perform a general tidy up

--- assets/js/endScene.js
+++ assets/js/endScene.js
@@ -3,7 +3,6 @@ class EndScene extends Phaser.Scene {
         super({ key: 'EndScene' });
     }
 
-
     preload() {
         this.load.image('end', 'assets/images/end-image.png');
     }
@@ -13,7 +12,7 @@ class EndScene extends Phaser.Scene {
         const replayText1 = this.add.text(35, 110, `Press any key`, {fontSize: '15px', fill: '#fff' });
         const replayText2 = this.add.text(35, 125, `to play again`, {fontSize: '15px', fill: '#fff' });
 
-        // Resets score to 0 for next round
+        // Resets score, money multiplier and speed for next round
         score = 0;
         moneyMultiplier = 100;
         speed = .4;
