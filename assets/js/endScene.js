68ebdb53e503bf05ec6b10aaca144db406ffa17e
Richard Byrne <richardbyrne1995@gmail.com>
2020-11-30 16:48:37 +0000

    Add $ to final score

--- assets/js/endScene.js
+++ assets/js/endScene.js
@@ -11,7 +11,7 @@ class EndScene extends Phaser.Scene {
         this.cameras.main.fadeIn(600);
 
         screen = this.add.image(0, 0, 'end').setOrigin(0).setScale(1.8);
-        const finalScore = this.add.text(40, 20, `Score: ${score}`, {fontSize: '17px', fill: '#fff'});
+        const finalScore = this.add.text(40, 20, `Score: $${score}`, {fontSize: '17px', fill: '#fff'});
         const replayText1 = this.add.text(35, 110, `Press any key`, {fontSize: '15px', fill: '#fff'});
         const replayText2 = this.add.text(35, 125, `to play again`, {fontSize: '15px', fill: '#fff'});
 
